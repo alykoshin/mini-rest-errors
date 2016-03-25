93f7fad20441fea2d55f18a7e62e078b3cc46697
Alexander <alykoshin@gmail.com>
2016-03-26 00:31:32 +0300

    added toString() to prototype in constructError

--- examples/index.js
+++ examples/index.js
@@ -7,6 +7,8 @@
 var appErrors = require('../').appErrors;
 var AppError = require('../').AppError;
 var HTTPError = require('../').HTTPError;
+var util = require('util');
+
 
 console.log('\n* HTTPError Example 1\n');
 
@@ -14,11 +16,13 @@ var httpError = new HTTPError(400);
 console.log('httpError:', httpError);
 console.log('httpError.stack:', httpError.stack);
 
+
 console.log('\n* HTTPError Example 2\n');
 
 var httpError = new HTTPError(400, 'This message overrides default');
 console.log('httpError:', httpError);
 console.log('httpError.stack:', httpError.stack);
+
 
 console.log('\n* AppError Example 1\n');
 
@@ -26,11 +30,13 @@ var appError = new AppError('notImplemented');
 console.log('appError:', appError);
 console.log('appError.stack:', appError.stack);
 
+
 console.log('\n* AppError Example 2\n');
 
 var appError = new AppError('notImplemented', 'more data');
 console.log('appError:', appError);
 console.log('appError.stack:', appError.stack);
+
 
 console.log('\n* AppError Example 3\n');
 
@@ -41,5 +47,14 @@ appErrors.newAppError = {
 
 var appError = new AppError('newAppError', 'more data');
 console.log('appError:', appError);
+console.log('appError.name:', appError.name);
+console.log('typeof appError:', typeof appError);
+console.log('appError instanceof Error:', appError instanceof Error);
+console.log('appError instanceof HTTPError:', appError instanceof HTTPError);
+console.log('appError instanceof AppError:', appError instanceof AppError);
+console.log('util.inspect(appError):', util.inspect(appError));
+console.log('JSON.stringify(appError,null,2):', JSON.stringify(appError,null,2));
+console.log('appError.toString():', appError.toString());
+console.log('\'\'+appError:', '' + appError);
 console.log('appError.stack:', appError.stack);
 
